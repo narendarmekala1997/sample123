cc0476255ef477e26a89351a69046c067941ba9c
narender <narendarmekala.9778@gmail.com>
2024-11-25 13:04:33 +0530

    Second Modification

--- src/sample123/Sample.java
+++ src/sample123/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 
 		System.out.println("This is a sample program");
+		System.out.println("I am modiifying for the seond time");
 	}
 
 }
